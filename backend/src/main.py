b4b1f4ddc25f571c7a6b52699b54fba521505ff6
Matheus Costa <matheus.bionicle@gmail.com>
2024-06-01 13:08:09 -0300

    feat: add oauth

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from auth.router import router as auth_router
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
 
+tags_metadata = [
+    {"name": "Users", "description": "Operations related to user management"}
+]
 
-@app.get("/")
-async def main_route():
-    return {"message": "Hello World"}
+app.include_router(auth_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
